cc5dcef1554d02aac925030488c98f9a685128c0
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-04 18:49:55 -0500

    Update sim.c

--- src/sim.c
+++ src/sim.c
@@ -4,8 +4,9 @@
 
 struct creature creature[2];
 srand(time(NULL));   // Initialization, should only be called once.
-int r = (rand() % 256) ;      // Returns a pseudo-random integer between 0 and RAND_MAX.
+
 int main() {
-	creature[0].genome[0] = (int) 
+	creature[0].genome[0] = (int) (rand() % 256);
+	printf("%d\n", rand() % 50);
 	return 0;
 }
