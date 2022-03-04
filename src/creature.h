42489fb1b63e17c0a78ad909ab580618d047dfcf
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-04 18:42:33 -0500

    Update creature.h

--- src/creature.h
+++ src/creature.h
@@ -2,11 +2,11 @@
 struct coord {
     int x;
     int y;
-};
+}coord;
 
 struct neuralnet {
     char placeholder; // figure this stuff out later
-};
+}neuralnet;
 
 struct creature {
     short genome[4]; 
