161b712ce13b9ef145d2324a14be5e159674ddd4
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-04 18:51:04 -0500

    Update sim.c

--- src/sim.c
+++ src/sim.c
@@ -4,9 +4,10 @@
 #include "creature.h"
 
 struct creature creature[2];
-srand(time(NULL));   // Initialization, should only be called once.
+
 
 int main() {
+	srand(time(NULL));   // Initialization, should only be called once.
 	creature[0].genome[0] = (int) (rand() % 256);
 	printf("%d\n", rand() % 50);
 	return 0;
