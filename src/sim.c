3eb1bda5f31ddb83f1283f9607d69f2395e92dd1
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-25 15:09:05 -0400

    Update sim.c

--- src/sim.c
+++ src/sim.c
@@ -1,15 +1,27 @@
-#include <time.h>
-#include <stdlib.h>
+#include <sodium.h>
 #include <stdio.h>
-#include "creature.h"
 
-struct creature creature[2];
+struct creature {
+    char matrix[64];
+    int loc;
+    //neuralnet neuralnet[4]
+    int genome[16];
+};
+
+int main()
+{   int genome[4];
+    int i = 0;
+
+    if (sodium_init() < 0) {
+        /* panic! the library couldn't be initialized, it is not safe to use */
+        return 1;
+    }
 
 
-
-int main() {
-	srand(time(NULL));   // Initialization, should only be called once.
-	creature[0].genome[0] = (int)(255 * ( (double)rand() / (double)RAND_MAX ));
-	printf("%d\n", rand() % 50);
-	return 0;
+    while(i < 3) {
+        genome[i] = randombytes_uniform(16);
+	    printf("%x",genome[i]);
+        i++;
+    }
+    printf("\n");
 }
