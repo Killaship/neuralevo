c78f44589ecf2611af90bd6ca6232394ede93934
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-25 15:18:24 -0400

    Update neuralevo.c

--- src/neuralevo.c
+++ src/neuralevo.c
@@ -1,6 +1,6 @@
 #include <sodium.h>
 #include <stdio.h>
-
+#define GENOME_SIZE 16
 struct creature {
     char matrix[64];
     int loc;
@@ -8,20 +8,19 @@ struct creature {
     int genome[16];
 };
 
-int main()
-{   int genome[4];
-    int i = 0;
+int main() {   
+	struct creature creature[1];
+	int i = 0;
+	int i2 = 0;
+	if (sodium_init() < 0) {
+        	/* panic! the library couldn't be initialized, it is not safe to use */
+        	return 1;
+	}
 
-    if (sodium_init() < 0) {
-        /* panic! the library couldn't be initialized, it is not safe to use */
-        return 1;
-    }
-
-
-    while(i < 4) {
-        genome[i] = randombytes_uniform(16);
-	    printf("%x",genome[i]);
-        i++;
-    }
-    printf("\n");
+	while(i < GENOME_SIZE) {
+		genome[i] = randombytes_uniform(16);
+		printf("%x",genome[i]);
+		i++;
+	}
+	printf("\n");
 }
